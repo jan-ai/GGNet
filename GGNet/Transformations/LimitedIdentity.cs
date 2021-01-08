f4ba4008befd879bf54b6a44401fb8440b924685
Jan Hesse <janhesse@hotmail.com>
2021-01-08 18:18:08 +0100

    new ITransformation class applying max and min limits on values

---
+++ GGNet/Transformations/LimitedIdentity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GGNet.Transformations
+{
+    public class LimitedIdentity<T> : ITransformation<T>
+        where T: struct, IComparable
+    {
+        private T? _min, _max;
+
+        public LimitedIdentity (T? min, T? max)
+        {
+            _min = min;
+            _max = max;
+        }
+
+        public T Apply(T value)
+        {
+            if (_min.HasValue && _min.Value.CompareTo(value) > 0)
+                value = _min.Value;
+
+            if (_max.HasValue && _max.Value.CompareTo(value) < 0)
+                value = _max.Value;
+
+            return value;
+        }
+
+        public T Inverse(T value) => value;
+    }
+}
